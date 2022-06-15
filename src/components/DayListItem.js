516338adc9f67f9a6b6e4f9b2990cc151eb6955e
Alex Cheung <alexc.sfu@gmail.com>
2022-06-14 18:31:31 -0700

    added unit test for the DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import classNames from 'classnames';
+import 'components/DayListItem.scss'
 
 export default function DayListItem(props) {
+
+  const itemClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots
+  })
+
+  const formatSpots = () => {
+    if(props.spots > 1) {
+      return `${props.spots} spots remaining`
+    } else if (props.spots === 1) {
+      return `${props.spots} spot remaining`
+    } else return 'no spots remaining';
+  };
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={itemClass} onClick={() => props.setDay(props.name)}>
       <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'>{props.spots} spots remaining</h3>
+      <h3 className='text--light'>{formatSpots()}</h3>
     </li>
   );
 };
