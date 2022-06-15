9d3f413f981116efed33727c42926b0b3c418191
Alex Cheung <alexc.sfu@gmail.com>
2022-06-14 19:43:16 -0700

    updated Application.js to added state for DayList component

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,7 +21,7 @@ const days = [
 ];
 
 export default function Application(props) {
-  const {day, setDay} = useState('Monday');
+  const [day, setDay] = useState('Monday');
 
   return (
     <main className="layout">
@@ -33,7 +33,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day='Monday' setDay={(day) => console.log(day)} />
+          <DayList days={days} day={day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
