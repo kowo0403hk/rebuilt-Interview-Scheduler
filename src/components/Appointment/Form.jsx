528e164bbeeb9b2999d28fb83f52242090414f96
Alex Cheung <alexc.sfu@gmail.com>
2022-06-16 12:37:59 -0700

    minor change on how Form.jsx receives props.interviewers object

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -3,7 +3,7 @@ import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
 export default function Form(props) {
-  // {state: student, interviewer || setStudent, setInterveiwer || onSave, onCancel}
+  // {student, interviewer(id), interviewers(object), onSave, onCancel}
 
   const [student, setStudent] = useState(props.student || '');
   const [interviewer, setInterveiwer] = useState(props.interviewer || null);
@@ -34,7 +34,7 @@ export default function Form(props) {
         </form>
         <InterviewerList 
           // code
-          interviewers={interviewers}
+          interviewers={props.interviewers}
           value={interviewer}
           onChange={setInterveiwer}
         />
@@ -48,11 +48,3 @@ export default function Form(props) {
     </main>
   );
 };
-
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
-];
